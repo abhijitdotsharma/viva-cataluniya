7eaab318f87ec3a350413bcd156d3bdf4a1c3220
wickedSharma <61442144+abhijitdotsharma@users.noreply.github.com>
2021-08-18 12:21:33 +0530

    create - added app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+let btnTranslate = document.querySelector("#btn-translate")
+let txtInput = document.querySelector('#txt-input')
+let outputDiv = document.querySelector('#output')
+
+let url = "https://api.funtranslations.com/translate/catalan.json"
+
+//construct the api call link
+function constructURL(text){
+    return url + "?text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something Wrong with server, Try again after some time .")
+}
+
+function clickHandler(){
+    //saving input for constructURL function call
+    let inputText = txtInput.value
+    
+    //Calling the Server
+    fetch(constructURL(inputText))
+        .then(response => response.json())
+        .then(json =>{
+                let translatedText = json.contents.translated
+                outputDiv.innerText = translatedText
+            })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
